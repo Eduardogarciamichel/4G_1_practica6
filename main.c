4c5602fed36d2a1218836b2bbbccb813c74752ec
hugop5 <vperezh447@gmail.com>
2018-06-06 15:37:07 -0500

    interrupcion timer0

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
 int cont=0x01;
-#INT_EXT
-VOID ISR_RB0(){
+#int_ext
+void isr_rb0(){
       if( cont==2){
         cont= 0x01;
       }
@@ -13,15 +13,27 @@ VOID ISR_RB0(){
       cont++; 
       }
 }
+int contadorTimer=1;
+#int_timer0
+void isr_timer0(){
+    contadorTimer<<=1;
+    if(contadorTimer>=8){
+        contadorTimer=1;
+    }
+}
 void main (void){
    setup_oscillator(OSC_16MHZ);
    set_tris_b(0x01);
    set_tris_e(0x00);
-   EXT_INT_EDGE(L_TO_H);
-     
+   set_tris_a(0x00);
+   ext_int_edge(L_TO_H);
+   set_timer0(0x00);
+   setup_timer_0(RTCC_INTERNAL |RTCC_DIV_128 | T0_8_BIT); 
+   enable_interrupts(INT_TIMER0);   
    enable_interrupts(INT_EXT);
    enable_interrupts(GLOBAL);
    while(1){
       output_e(cont);    
+      output_a(contadorTimer);
    } 
 }   
