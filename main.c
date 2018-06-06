190c6dba46bca403b11181bd897815e1aa6c0f3f
LeitzaJmz <yoshia_98@hotmail.com>
2018-06-06 15:38:23 -0500

    Fast_IO

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
 #include <18F4620.h>
+#include <stdio.h>
+#include <stdlib.h>
+#use fast_io(a)
+#use fast_io(d)
+#use fast_io(b)
+#use fast_io(e)
 #fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
+#BIT dataDht = 0xF82.4             //pin de dato de entrada del DHT11 portc
+#BIT dht_io = 0xF94.4  //bit 0 del tris b para ponerlo como entrada o salida
 int cont=0x01;
 #INT_EXT
-VOID ISR_RB0(){
-      if( cont==2){
-        cont= 0x01;
-      }
-      else
-      {
+void ISR_RB0(){
+   if( cont==2){
+      cont= 0x01;
+   }
+   else{
       cont++; 
-      }
+   }
 }
 void main (void){
    setup_oscillator(OSC_16MHZ);
